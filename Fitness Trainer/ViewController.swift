69e37f76565f7c9fc2990f359cc79b08d5b05246
Bryan Hernandez <61296527+bryhernandez@users.noreply.github.com>
2021-10-25 09:52:31 -0700

    Initial Commit

---
+++ Fitness Trainer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Fitness Trainer
+//
+//  Created by Bryan Hernandez on 10/25/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
