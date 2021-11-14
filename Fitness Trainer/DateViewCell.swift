506f0dc36741b10ca8ca62f85c54f99c0365505a
Oscar Ruiz <Oscarruiz25341@gmail.com>
2021-11-14 14:48:01 -0800

    adding files and uis

---
+++ Fitness Trainer/DateViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  DateViewCell.swift
+//  Fitness Trainer
+//
+//  Created by Oscar on 11/14/21.
+//
+
+import UIKit
+
+class DateViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Fitness Trainer/ViewScheduleCell.swift
@@ -0,0 +1,23 @@
+//
+//  ViewScheduleCell.swift
+//  Fitness Trainer
+//
+//  Created by Oscar on 11/14/21.
+//
+
+import UIKit
+
+class ViewScheduleCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
