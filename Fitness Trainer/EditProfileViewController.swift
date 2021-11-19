94209279a41752c0386e6d00d2523fdd33ba3d00
Bryan Hernandez <61296527+bryhernandez@users.noreply.github.com>
2021-11-19 11:21:16 -0800

    Added labels to show Users current information. Button to edit does not work yet.

--- Fitness Trainer/EditProfileViewController.swift
+++ Fitness Trainer/EditProfileViewController.swift
@@ -6,16 +6,41 @@
 //
 
 import UIKit
+import Parse
 
 class EditProfileViewController: UIViewController {
+    
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var passwordLabel: UILabel!
+    @IBOutlet weak var ageLabel: UILabel!
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        let user = PFUser.current()!
+        let age = user["Age"] as! Int?
+        let height = user["Height"] as! Int?
+        let weight = user["Weight"] as! Int?
+        
+        print(user.password)
+        
+        usernameLabel.text = user.username
+//        passwordLabel.text = user.password
+        ageLabel.text = String(Int(age!))
+        heightLabel.text = String(Int(height!))
+        weightLabel.text = String(Int(weight!))
+        
     }
     
-
+    @IBAction func onEdit(_ sender: Any) {
+        
+    }
+    
+    
+    
     /*
     // MARK: - Navigation
 
