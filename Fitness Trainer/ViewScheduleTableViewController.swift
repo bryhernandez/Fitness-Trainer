652c15395a3a0de1fdb2c05c9ce25cfe443477d8
Bryan Hernandez <61296527+bryhernandez@users.noreply.github.com>
2021-11-23 21:26:28 -0800

    Cleaned up ViewSchedule and made buttons look nicer.

--- Fitness Trainer/ViewScheduleTableViewController.swift
+++ Fitness Trainer/ViewScheduleTableViewController.swift
@@ -11,50 +11,27 @@ import Parse
 class ViewScheduleTableViewController: UITableViewController {
     
     var plan = [PFObject]()
-    var size:Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
 
     // MARK: - Table view data source
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ViewScheduleCell", for: indexPath) as! ViewScheduleCell
-        let user = PFUser.current()!
         let the_plan = plan[indexPath.row]
-        let id = user.username as! String?
-        let query = PFQuery(className:"Workouts")
-        
+
         let sets = the_plan["Sets"] as! Int?
         let reps = the_plan["Reps"] as! Int?
         cell.dateLabel.text = the_plan["Date"] as? String
         cell.workoutLabel.text = the_plan["Name"] as? String
         cell.setsLabel.text = String(Int(sets!))
         cell.repsLabel.text = String(Int(reps!))
-        
-        query.whereKey("username", equalTo: id)
-        query.findObjectsInBackground { (objects: [PFObject]?, error: Error?) in
-            if let error = error {
-                // Log details of the failure
-                print(error.localizedDescription)
-            }
-            else if let objects = objects {
-                // The find succeeded.
-                print("Successfully retrieved \(objects.count) workouts with the user.")
-            
-                // Do something with the found objects
-            
-            }
-        }
+    
         return cell
     }
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
         return 1
@@ -70,7 +47,7 @@ class ViewScheduleTableViewController: UITableViewController {
         
         let user = PFUser.current()!
         let id = user.username as! String?
-        let query = PFQuery(className:"Workouts")
+        let query = PFQuery(className: "Workouts")
         query.whereKey("username", equalTo: id)
         query.findObjectsInBackground { (workouts, error) in
             if workouts != nil{
